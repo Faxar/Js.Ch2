d2b2718cce05881ef9c03902969084a5e71bd8b9
Faxar <alazerus@gmail.com>
2018-10-10 22:34:01 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function Question(question, correctAnswer) {
+    this.question = question;
+    this.answer = correctAnswer;
+}
+
+var firstQuestion = new Question('What is your name?', 'Vassili');
+var secondQuestion = new Question('How old are you?', '34');
+
+var questionsArr = [firstQuestion, secondQuestion];
+
+(
+    for (var i = 0; i<questionsArr.length; i++){
+
+    }
+)
+
+
+
